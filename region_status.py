8876319bb163d5a2486a858b82ede069894f0ba2
Beau Uriona <buriona@usbr.gov>
2020-01-08 11:15:07 -0700

    initial commit

---
+++ region_status.py
@@ -0,0 +1,494 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 06:55:24 2020
+
+@author: buriona
+"""
+
+import re
+from io import StringIO
+from datetime import datetime as dt
+from os import path
+from requests import get as r_get
+import folium
+import branca
+from folium.plugins import FloatImage, MousePosition
+from folium.features import DivIcon
+import pandas as pd
+# import geopandas as gpd
+# from shapely.geometry import Point
+# from shapely.ops import cascaded_union
+from region_status_utils import get_fa_icon, get_icon_color, get_season
+from region_status_utils import add_optional_tilesets, add_huc_layer, get_huc
+from region_status_utils import get_favicon, get_bor_seal
+from region_status_utils import get_bor_js, get_bor_css
+from region_status_utils import get_default_js, get_default_css
+
+bor_js = get_bor_js()
+bor_css = get_bor_css()
+
+default_js = get_default_js()
+default_css = get_default_css()
+
+folium.folium._default_js = default_js
+folium.folium._default_css = default_css
+# folium.folium._default_js = bor_js
+# folium.folium._default_css = bor_css
+
+nrcs_url = 'https://www.nrcs.usda.gov/Internet/WCIS/basinCharts/POR'
+regions = {
+    'Arkansas-White-Red': {
+        'coords': [37, -103]
+    }, 
+    'California': {
+        'coords': [37.5, -123.5]
+    },
+    'Great Basin': {
+        'coords': [40.75, -117]
+    },
+    'Lower Colorado': {
+        'coords': [35, -113]
+    },
+    'Missouri': {
+        'coords': [46.7, -106.2]
+    },
+    'Pacific Northwest': {
+        'coords': [45, -117.5]
+    },
+    'Rio Grande':{
+        'coords': [35.2, -107.7]
+    },
+    'Upper Colorado': {
+        'coords': [39.5, -110.7]
+    }
+}
+
+reservoirs = {
+    'Canyon Ferry': {
+        'coords': [46.487414, -111.544201], 'region': 'gp', 'anno': 1, 'cap': 1891.888, 'id': 'cfr'
+    },
+    'Bighorn': {
+        'coords': [44.848426, -108.179062], 'region': 'gp', 'anno': 1, 'cap': 1020.573, 'id': 'bhr'
+    },
+    'Shadehill Reservoir': {
+        'coords': [45.729203, -102.256583], 'region': 'gp', 'anno': 1, 'cap': 120.172, 'id': 'shr'
+    },
+    'Angostura Reservoir': {
+        'coords': [43.308556, -103.422114], 'region': 'gp', 'anno': 1, 'cap': 123.048, 'id': 'agr'
+    },
+    'Palisades Reservoir': {
+        'coords': [43.194831, -111.081225], 'region': 'pn', 'anno': 1, 'cap': 1200, 'id': 'pal'
+    },
+    'Arrowrock Reservoir': {
+        'coords': [43.604093, -115.858169], 'region': 'pn', 'anno': 1, 'cap': 272.2, 'id': 'ark'
+    },
+    'Trinity Dam': {
+        'coords': [40.969630, -122.676816], 'region': 'mp', 'anno': 1, 'cap': 2447.7, 'id': 'cle'
+    },
+    'Shasta Lake': {
+        'coords': [40.735191, -122.414965], 'region': 'mp', 'anno': 1, 'cap': 4552, 'id': 'sha'
+    },
+    'Folsom Lake': {
+        'coords': [38.729518, -121.119817], 'region': 'mp', 'anno': 1, 'cap': 977, 'id': 'fol'
+    },
+    'Millerton Lake': {
+        'coords': [37.004422, -119.690673], 'region': 'mp', 'anno': 1, 'cap': 520.5, 'id': 'mil'
+    },
+    'Flaming Gorge': {
+        'coords': [41.093419, -109.540657], 'region': 'uc', 'anno': 1, 'cap': 3749, 'id': 1718
+    },
+    'Lake Powell': {
+        'coords': [37.068847, -111.243924], 'region': 'uc', 'anno':2, 'cap': 24322, 'id': 1719
+    },
+    'Lake Mead': {
+        'coords': [36.145605, -114.41476], 'region': 'lc', 'anno': 2, 'cap': 28945, 'id': 1721
+    },
+    'Elephant Butte': {
+        'coords': [33.251770, -107.166321], 'region': 'uc', 'anno': 2, 'cap': 1973.358, 'id': 2684
+    },
+}
+
+forecasts = {
+    'Grand Coulee': {
+        'coords': [47.952807, -118.980527], 'region': 'pn', 'anno': 1, 'avg': 56763, 'id': 'GCDW1'
+    },
+    'Columbia River': {
+        'coords': [45.604048, -121.173913], 'region': 'pn', 'anno': 1, 'avg': 87532, 'id': 'TDAO3'
+    }
+}
+
+def get_uc_data(sdi):
+    base_url = 'https://www.usbr.gov/pn-bin/hdb/hdb.pl?svr=uchdb2'
+    sdi = f'&sdi={sdi}'
+    tstp = '&tstp=DY'
+    now = dt.now()
+    dt_t1 = dt(now.year - 1, now.month, now.day).strftime('%Y-%m-%dT00:00')
+    t1 = f'&t1={dt_t1}'
+    dt_t2 = dt(now.year, now.month, now.day).strftime('%Y-%m-%dT00:00')
+    t2 = f'&t2={dt_t2}'
+    suffix= '&table=R&mrid=0&format=88'
+    request_url = f'{base_url}{sdi}{tstp}{t1}{t2}{suffix}'
+    df = pd.read_csv(request_url, index_col='Date', parse_dates=True)
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url.replace('csv', 'html')
+    }
+
+def get_lc_data(sdi):
+    base_url = 'https://www.usbr.gov/pn-bin/hdb/hdb.pl?svr=lchdb2'
+    sdi = f'&sdi={sdi}'
+    tstp = '&tstp=DY'
+    now = dt.now()
+    dt_t1 = dt(now.year - 1, now.month, now.day).strftime('%Y-%m-%dT00:00')
+    t1 = f'&t1={dt_t1}'
+    dt_t2 = dt(now.year, now.month, now.day).strftime('%Y-%m-%dT00:00')
+    t2 = f'&t2={dt_t2}'
+    suffix= '&table=R&mrid=0&format=88'
+    request_url = f'{base_url}{sdi}{tstp}{t1}{t2}{suffix}'
+    df = pd.read_csv(request_url, index_col='Date', parse_dates=True)
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url.replace('csv', 'html')
+    }
+
+def get_pn_data(site_id):
+    base_url = 'https://www.usbr.gov/pn-bin/daily.pl'
+    station = f'?station={site_id}'
+    frmt = '&format=csv'
+    now = dt.now()
+    dt_t1 = dt(now.year - 1, now.month, now.day)
+    s_date = f'&year={dt_t1.year}&month={dt_t1.month}&day={dt_t1.day}'
+    dt_t2 = dt(now.year, now.month, now.day)
+    e_date = f'&year={dt_t2.year}&month={dt_t2.month}&day={dt_t2.day}'
+    pcode = '&pcode=af'
+    request_url = f'{base_url}{station}{frmt}{s_date}{e_date}{pcode}'
+    df = pd.read_csv(request_url, index_col='DateTime', parse_dates=True)
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url.replace('csv', 'html')
+    }
+
+def get_gp_data(site_id):
+    base_url = 'https://www.usbr.gov/gp-bin/webarccsv.pl'
+    param = f'?parameter={site_id.upper()}%20AF'
+    now = dt.now()
+    dt_t1 = dt(now.year - 1, now.month, now.day)
+    s_date = f'&syer={dt_t1.year}&smnth={dt_t1.month}&sdy={dt_t1.day}'
+    dt_t2 = dt(now.year, now.month, now.day)
+    e_date = f'&eyer={dt_t2.year}&emnth={dt_t2.month}&edy={dt_t2.day}'
+    frmt = '&format=4'
+    request_url = f'{base_url}{param}{s_date}{e_date}{frmt}'
+    df = pd.read_csv(
+        request_url, index_col='#DATE', parse_dates=True, na_values='MISSING'
+    )
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url.replace('format=4', 'format=3')
+    }
+
+def get_mp_data(site_id):
+    base_url = 'http://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet'
+    station = f'?Stations={site_id.upper()}'
+    sensor = '&SensorNums=15'
+    duration = '&dur_code=D'
+    now = dt.now()
+    t1 = dt(now.year - 1, now.month, now.day).strftime('%Y-%m-%d')
+    s_date = f'&Start={t1}'
+    t2 = dt(now.year, now.month, now.day).strftime('%Y-%m-%d')
+    e_date = f'&End={t2}'
+    request_url = f'{base_url}{station}{sensor}{duration}{s_date}{e_date}'
+    df_all = pd.read_csv(
+        request_url, index_col='DATE TIME', parse_dates=True, na_values='---'
+    )
+    df = df_all[['VALUE']].copy()
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url
+    }
+
+def get_frcst_data(site_id):
+    base_url = 'https://www.nwrfc.noaa.gov/water_supply/ws_text.cgi'
+    station = f'?id={site_id.upper()}'
+    now = dt.now()
+    wy = now.year
+    if now.month > 9:
+        wy += 1
+    wy_str = f'&wy={wy}'
+    period = '&per=APR-AUG'
+    frcst_type = '&type=ESP10'
+    prob = '&prob=0'
+    request_url = f'{base_url}{station}{wy_str}{period}{frcst_type}{prob}'
+    request_txt = r_get(request_url).text
+    request_txt = request_txt.replace('<br>', '\n')
+    request_txt = request_txt.replace('</pre></body>', '')
+    request_io = StringIO()
+    request_io.write(request_txt)
+    request_io.seek(0)
+    df_all = pd.read_csv(
+        request_io, skiprows=4, index_col='Issuance Date', 
+        parse_dates=True, na_values=''
+    )
+    df = df_all[['50% FCST']].copy()
+    df = df.astype(float)
+    df.dropna(inplace=True)
+    df.index = pd.to_datetime(df.index)
+    df.sort_index(inplace=True)
+    return {
+        'data': df.iloc[-1,0], 
+        'dt': df.index[-1].to_pydatetime(),
+        'url': request_url
+    }
+
+def get_embed(href):
+    embed = (
+        f'<div class="container embed-responsive embed-responsive-4by3" style="overflow: hidden; height: 650px; width: 720px;">'
+        f'<iframe class="embed-responsive-item" src="{href}" scrolling="no" frameborder="0" allowfullscreen></iframe>'
+        f'</div>'
+    )   
+    return embed
+
+def add_region_markers(rs_map, regions=regions, nrcs_url=nrcs_url):
+    for region, region_meta in regions.items():
+        print(f'  Adding {region} to the map...')
+        swe_url = f'{nrcs_url}/WTEQ/assocHUC2/{region} Region.html'
+        prec_url = f'{nrcs_url}/PREC/assocHUC2/{region} Region.html'
+        swe_txt = r_get(swe_url).text
+        prec_txt = r_get(prec_url).text
+        swe_regex = r"(?<=% of Median - )(.*)(?=%<br>%)"
+        prec_regex = r"(?<=% of Average - )(.*)(?=%<br>%)"
+    
+        swe_re = re.search(swe_regex, swe_txt, re.MULTILINE)
+        prec_re = re.search(prec_regex, prec_txt, re.MULTILINE)
+        swe_percent = swe_txt[swe_re.start():swe_re.end()]
+        prec_percent = prec_txt[prec_re.start():prec_re.end()]
+        seasonal_url = swe_url
+        other_season_url = prec_url
+        other_chart_type = 'Precip.'
+        if get_season() == 'summer':
+            seasonal_url = prec_url
+            other_season_url = swe_url
+            other_chart_type = 'Snow'
+            
+        popup_html = (
+                f'<div class="container">'
+                f'<div class="row justify-content-center">'
+                f'<div class="col">'
+                f'<a href="{prec_url}" target="_blank">'
+                f'<button class="btn btn-primary btn-sm btn-block">'
+                f'Go to {region} {other_chart_type} Chart...</button></a></div>'
+                f'<div class="row justify-content-center">{get_embed(seasonal_url)}</div>'
+                f'</div></div>'
+            )
+        popup = folium.map.Popup(html=popup_html)
+            
+        marker_label = f'''
+        <button type="button" class="btn btn-primary">
+          <span style="white-space: nowrap;">{region}</span><br>
+          <span style="white-space: nowrap;">
+            {prec_percent}% <i class="fa fa-umbrella"></i>
+            {swe_percent}% <i class="fa fa-snowflake-o"></i>
+          </span>
+        </button>
+        '''
+        
+        div_icon = DivIcon(
+            icon_anchor=(0,0),
+            html=marker_label,
+        )
+        folium.Marker(
+            location=region_meta['coords'],
+            popup=popup,
+            tooltip='Click for chart.',
+            icon=div_icon
+        ).add_to(rs_map)
+
+def add_res_markers(rs_map, reservoirs=reservoirs):
+    for res_name, res_meta in reservoirs.items():
+        print(f'  Adding {res_name} to map...')
+        if res_meta['region'] == 'uc':
+            current_data = get_uc_data(res_meta['id'])
+        elif res_meta['region'] == 'lc':
+            current_data = get_lc_data(res_meta['id'])
+        elif res_meta['region'] == 'pn':
+            current_data = get_pn_data(res_meta['id'])
+        elif res_meta['region'] == 'gp':
+            current_data = get_gp_data(res_meta['id'])
+        elif res_meta['region'] == 'mp':
+            current_data = get_mp_data(res_meta['id'])
+        else:
+            current_data = None
+        
+        if current_data:
+            percent_cap = 100 * round(current_data['data'] / (1000 * res_meta['cap']), 2)
+            percent_cap = f'{percent_cap:0.0f}'
+            tooltip = f"As of: {current_data['dt'].strftime('%x')}"
+        else:
+            percent_cap = 'N/A'
+            tooltip = 'Error retrieving data!'
+        anno = res_meta['anno']
+        url = current_data['url']
+        marker_label = f'''
+        <a href="{url}" target="_blank">
+          <button type="button" class="btn btn-sm btn-info">
+            <span style="white-space: nowrap;">{res_name}</span><br>
+            <span style="white-space: nowrap;">
+              {percent_cap}%<sup>{anno}</sup> <i class="fa fa-tint"></i>
+            </span>
+          </button>
+        </a>
+        '''
+        icon_anchor = (0,0)
+        if res_name.lower() in ['elephant butte', 'trinity dam']:
+            icon_anchor = (80,35)
+            
+        div_icon = DivIcon(
+            # icon_size=(120,40),
+            icon_anchor=icon_anchor,
+            html=marker_label,
+        )
+        
+        folium.Marker(
+            location=res_meta['coords'],
+            # popup=popup,
+            tooltip=tooltip,
+            icon=div_icon
+        ).add_to(rs_map)
+
+def add_frcst_markers(rs_map, forecasts=forecasts):
+    for frcst_name, frcst_meta in forecasts.items():
+        print(f'  Adding {frcst_name} to map...')
+        
+        if frcst_meta['region'] == 'pn':
+            current_data = get_frcst_data(frcst_meta['id'])
+        else:
+            current_data = None
+        
+        if current_data:
+            percent_cap = 100 * round(current_data['data'] / (frcst_meta['avg']), 2)
+            percent_cap = f'{percent_cap:0.0f}'
+            tooltip = f"As of: {current_data['dt'].strftime('%x')}"
+        else:
+            percent_cap = 'N/A'
+            tooltip = 'Error retrieving data!'         
+        anno = frcst_meta['anno']    
+        url = current_data['url']
+        marker_label = f'''
+        <a href="{url}" target="_blank">
+          <button type="button" class="btn btn-sm btn-info">
+            <span style="white-space: nowrap;">{frcst_name}</span><br>
+            <span style="white-space: nowrap;">
+              Apr-Aug Forecast
+            </span>
+            <span style="white-space: nowrap;">
+              {percent_cap}%<sup>{anno}</sup> <i class="fa fa-tachometer"></i>
+            </span>
+          </button>
+        </a>
+        '''
+
+        div_icon = DivIcon(
+            # icon_size=(120,40),
+            icon_anchor=(0,0),
+            html=marker_label,
+        )
+        
+        folium.Marker(
+            location=frcst_meta['coords'],
+            # popup=popup,
+            tooltip=tooltip,
+            icon=div_icon
+        ).add_to(rs_map)
+        
+if __name__ == '__main__':
+    
+    this_dir = path.dirname(path.realpath(__file__))
+    map_dir = path.join(this_dir, 'maps')
+    gis_dir = path.join(this_dir, 'gis')
+    
+    rs_map = folium.Map(
+            tiles=None,
+            location=[39.5, -110],
+            zoom_start=5.5
+        )
+    add_huc_layer(rs_map, 2)
+    add_optional_tilesets(rs_map)
+    folium.LayerControl().add_to(rs_map)
+    FloatImage(
+        get_bor_seal(orient='horz'),
+        bottom=78,
+        left=1
+    ).add_to(rs_map)
+    # MousePosition(prefix="Location: ").add_to(rs_map)
+    map_path = path.join(map_dir, 'regional_status.html')
+    
+    print('Adding Regional Forecast markers...')
+    add_frcst_markers(rs_map)
+    print('Adding Regional Reservoir markers...')
+    add_res_markers(rs_map)
+    print('Adding Regional PREC/SWE markers...')
+    add_region_markers(rs_map)
+    all_coords = [i['coords'] for i in reservoirs.values()]
+    all_coords = all_coords + [i['coords'] for i in forecasts.values()]
+    all_coords = all_coords + [i['coords'] for i in regions.values()]
+    rs_map.fit_bounds(all_coords)
+    update_date = dt.now().strftime('%B %d, %Y')
+    footer_html = f'''
+    <div style="position: fixed; bottom: 205px; left: 30px; z-index:9999; font-size:26px;">
+      <b>Reclamation West-Wide Summary</b><br>
+    </div>
+    <div style="position: fixed; bottom: 180px; left: 30px; z-index:9999; font-size:22px;">
+        Precipitation and Storage Figures<br>
+    </div>
+    <div style="position: fixed; bottom: 30px; left: 30px; z-index:9999; font-size:13px;">
+      <sup>1</sup>Storage percent of capacity more sensitive to seasonal flows<br>
+      <sup>2</sup>Storage percent of capacity less sensitive to seasonal flows<br>
+      <i class="fa fa-umbrella"></i>&nbsp Water year-to-date precipitation (precip) provided as % of average<br>
+      <i class="fa fa-snowflake-o"></i>&nbsp Snow water equivalent (snow) provided as % of median<br>
+      <i class="fa fa-tint"></i>&nbsp Reservoir storage provided as percentage of capacity<br>
+      <i class="fa fa-tachometer"></i>&nbsp Forecast volumes provided as percentage of 30-yr average<br>
+      Precipitation, SWE, and reservoir data available from 
+      <a href="https://www.wcc.nrcs.usda.gov/">NRCS</a>/
+      <a href="https://www.usbr.gov/">BOR</a>/
+      <a href="https://cdec.water.ca.gov/">CDEC</a><br>
+      Updated as of {update_date}
+    </div>
+    '''
+    footer = folium.Element(footer_html)
+    rs_map.get_root().html.add_child(footer)
+    rs_map.save(map_path)
+    flavicon = (
+        f'<link rel="shortcut icon" '
+        f'href="{get_favicon()}"></head>'
+    )
+    with open(map_path, 'r') as html_file:
+        chart_file_str = html_file.read()
+
+    with open(map_path, 'w') as html_file:
+        chart_file_str = chart_file_str.replace(r'</head>', flavicon)
+        replace_str = (
+            '''left:1%;
+                    max-width:20%;
+                    max-height:20%;'''
+        )
+        chart_file_str = chart_file_str.replace(r'left:1%;', replace_str)
+        html_file.write(chart_file_str)
+    
+    
+        
---
+++ region_status_utils.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 15:26:04 2019
+
+@author: buriona
+"""
+
+from os import path
+import folium
+import pandas as pd
+# import geopandas as gpd
+from shapely.geometry import Point
+from datetime import datetime
+
+STATIC_URL = f'https://www.usbr.gov/uc/water/ff/assets'
+
+def get_plotly_js():
+    return f'{STATIC_URL}/plotly.js'
+
+def get_favicon():
+    return f'{STATIC_URL}/img/favicon.ico'
+
+def get_bootstrap():
+    return {
+        'css': f'{STATIC_URL}/bootstrap/css/bootstrap.min.css',
+        'js': f'{STATIC_URL}/bootstrap/js/bootstrap.bundle.js',
+        'jquery': f'{STATIC_URL}/jquery.js',
+        'popper': f'{STATIC_URL}/popper.js',
+        'fa': f'{STATIC_URL}/font-awesome/css/font-awesome.min.css',
+    }
+
+def get_bor_seal(orient='default', grey=False):
+    color = 'cmyk'
+    if grey:
+        color = 'grey'
+    seal_dict = {
+        'default': f'BofR-horiz-{color}.png',
+        'shield': f'BofR-shield-cmyk.png',
+        'vert': f'BofR-vert-{color}.png',
+        'horz': f'BofR-horiz-{color}.png'
+        }
+    return f'{STATIC_URL}/img/{seal_dict[orient]}'
+
+def get_bor_js():
+    return [
+        ('leaflet',
+          f'{STATIC_URL}/js/leaflet/leaflet.js'),
+        ('jquery',
+          f'{STATIC_URL}/js/jquery/3.4.0/jquery.min.js'),
+        ('bootstrap',
+          f'{STATIC_URL}/js/bootstrap/3.2.0/js/bootstrap.min.js'),
+        ('awesome_markers',
+          f'{STATIC_URL}/js/leaflet/leaflet.awesome-markers.js'),  # noqa
+        ]
+
+def get_bor_css():
+    return [
+        ('leaflet_css',
+          f'{STATIC_URL}/css/leaflet/leaflet.css'),
+        ('bootstrap_css',
+          f'{STATIC_URL}/css/bootstrap/3.2.0/css/bootstrap.min.css'),
+        ('bootstrap_theme_css',
+          f'{STATIC_URL}/css/bootstrap/3.2.0/css/bootstrap-theme.min.css'),  # noqa
+        ('awesome_markers_font_css',
+          f'{STATIC_URL}/css/font-awesome.min.css'),  # noqa
+        ('awesome_markers_css',
+          f'{STATIC_URL}/css/leaflet/leaflet.awesome-markers.css'),  # noqa
+        ('awesome_rotate_css',
+          f'{STATIC_URL}/css/leaflet/leaflet.awesome.rotate.css'),  # noqa
+        ]
+
+def get_default_js():
+    bootstrap_dict = get_bootstrap()
+    return [
+        ('leaflet', 
+         f'{STATIC_URL}/leaflet/js/leaflet.js'),
+        ('jquery', 
+         bootstrap_dict['jquery']),
+        ('bootstrap', 
+         bootstrap_dict['js']),
+        ('awesome_markers', 
+         f'{STATIC_URL}/leaflet-awesome-markers/leaflet.awesome-markers.min.js'),
+        ('popper', 
+         bootstrap_dict['popper']),
+    ]
+
+def get_default_css():
+    bootstrap_dict = get_bootstrap()
+    return [
+        ('leaflet_css', 
+         f'{STATIC_URL}/leaflet/css/leaflet.css'),
+        ('bootstrap_css', 
+         bootstrap_dict['css']),
+        ('awesome_markers_font_css', 
+          bootstrap_dict['fa']),
+        ('awesome_markers_css', 
+        f'{STATIC_URL}/leaflet-awesome-markers/leaflet.awesome-markers.css'),
+        ('awesome_rotate_css', 
+         f'{STATIC_URL}/leaflet-awesome-markers/leaflet.awesome.rotate.css'),
+    ]
+
+def get_fa_icon(obj_type='default', source='hdb'):
+    if source.lower() == 'hdb':
+        fa_dict = {
+            'default': 'map-pin',
+            1: 'sitemap',
+            2: 'umbrella',
+            3: 'arrow-down',
+            4: 'exchange',
+            5: 'plug',
+            6: 'arrows-v',
+            7: 'tint',
+            8: 'snowflake-o',
+            9: 'tachometer',
+            10: 'cogs',
+            11: 'arrows-h',
+            12: 'rss',
+            13: 'flask',
+            14: 'table',
+            15: 'info',
+            20: 'exchange'
+        }
+    if source.lower() == 'awdb':
+        fa_dict = {
+            'default': 'map-pin',
+            'SCAN': 'umbrella',
+            'PRCP': 'umbrella',
+            'BOR': 'tint',
+            'SNTL': 'snowflake-o',
+            'SNOW': 'snowflake-o',
+            'SNTLT': 'snowflake-o',
+            'USGS': 'tachometer',
+            'MSNT': 'snowflake-o',
+            'MPRC': 'umbrella'
+        }
+    fa_icon = fa_dict.get(obj_type, 'map-pin')
+    return fa_icon
+
+def get_icon_color(row, source='hdb'):
+    if source.lower() == 'hdb':
+        obj_owner = 'BOR'
+        if not row.empty:
+            if row['site_metadata.scs_id']:
+                obj_owner = 'NRCS'
+            if row['site_metadata.usgs_id']:
+                obj_owner = 'USGS'
+    if source.lower() == 'awdb':
+        obj_owner = row
+    color_dict = {
+        'BOR': 'blue',
+        'NRCS': 'red',
+        'USGS': 'green',
+        'COOP': 'gray',
+        'SNOW': 'darkred',
+        'PRCP': 'lightred',
+        'SNTL': 'red',
+        'SNTLT': 'lightred',
+        'SCAN': 'lightred',
+        'MSNT': 'orange',
+        'MPRC': 'beige',
+        
+    }
+    icon_color = color_dict.get(obj_owner, 'black')
+    return icon_color
+
+def add_optional_tilesets(folium_map):
+    tilesets = {
+        "Terrain": 'Stamen Terrain',
+        'Street Map': 'OpenStreetMap',
+        'Toner': 'Stamen Toner',
+        'Watercolor': 'Stamen Watercolor',
+        'Positron': 'CartoDB positron',
+        'Dark Matter': 'CartoDB dark_matter',
+    }
+    for name, tileset in tilesets.items():
+        folium.TileLayer(tileset, name=name).add_to(folium_map)
+
+def add_huc_layer(huc_map, level=2, huc_geojson_path=None, embed=False):
+    try:
+        if not huc_geojson_path:
+            huc_geojson_path = f'{STATIC_URL}/gis/HUC{level}.geojson'
+        huc_style = lambda x: {
+            'fillColor': '#ffffff00', 'color': '#1f1f1faa', 'weight': 2
+        }
+        show = False
+        if level == 2:
+            show = True
+        folium.GeoJson(
+            huc_geojson_path,
+            name=f'HUC {level}',
+            embed=embed,
+            style_function=huc_style,
+            show=show
+        ).add_to(huc_map)
+    except Exception as err:
+        print(f'Could not add HUC {level} layer to map! - {err}')
+
+def clean_coords(coord_series, force_neg=False):
+    
+    coord_series = coord_series.apply(
+        pd.to_numeric, 
+        errors='ignore', 
+        downcast='float'
+    )
+    if not coord_series.apply(type).eq(str).any():
+        if force_neg:
+            return -coord_series.abs()
+        return coord_series
+    results = []
+    for idx, coord in coord_series.iteritems():
+        if not str(coord).isnumeric():
+            coord_strs = str(coord).split(' ')
+            coord_digits = []
+            for coord_str in coord_strs:
+                coord_digit = ''.join([ch for ch in coord_str if ch.isdigit() or ch == '.'])
+                coord_digits.append(float(coord_digit))
+            dec = None
+            coord_dec = 0
+            for i in reversed(range(0, len(coord_digits))):
+                if dec:
+                    coord_dec = abs(coord_digits[i]) + dec
+                dec = coord_digits[i] / 60
+            if str(coord)[0] == '-':
+                coord_dec = -1 * coord_dec
+            results.append(coord_dec)
+        else:
+            results.append(coord)
+    if force_neg:
+        results[:] = [-1 * result if result > 0 else result for result  in results]
+    clean_series = pd.Series(results, index=coord_series.index)
+    return clean_series
+
+def get_huc(geo_df, lat, lon, level='12'):
+
+    for idx, row in geo_df.iterrows():
+        polygon = row['geometry']
+        point = Point(lon, lat)
+        if polygon.contains(point):
+            return row[f'HUC{level}']
+    return None
+
+def get_season():
+    curr_month = datetime.now().month
+    if curr_month > 3:
+        return 'spring'
+    if curr_month > 5:
+        return 'summer'
+    if curr_month > 10:
+        return 'fall'
+    return 'winter'
+
+if __name__ == '__main__':
+    print('Just a utility module')
